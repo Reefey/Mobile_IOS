dd9e53930fbe5d273639c4741037864cdd54602a
Reza Juliandri <rezajuliandri20@gmail.com>
2025-08-24 10:52:32 +0800

    feat: implementation collections and detail

---
+++ AppExperiment/Enums/CollectionViewTypeEnum.swift
@@ -0,0 +1,10 @@
+//
+//  CollectionGridTypeEnum.swift
+//  AppExperiment
+//
+//  Created by Reza Juliandri on 23/08/25.
+//
+enum CollectionViewTypeEnum: String, CaseIterable{
+    case GRID = "Grid"
+    case LIST = "List"
+}
